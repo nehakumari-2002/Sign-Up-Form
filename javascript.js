cd6d0302c1c56991557f9db80ea4cf1e3b047e47
nehakumari-2002 <131653801+nehakumari-2002@users.noreply.github.com>
2024-05-06 20:41:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let signUpBtn= document.querySelector('.signupbtn');
+let signInBtn= document.querySelector('.signinbtn');
+let nameField= document.querySelector('.namefield');
+let title= document.querySelector('.title');
+let underline= document.querySelector('.underline');
+let text= document.querySelector('.text');
+
+signInBtn.addEventListener('click',()=>{
+   nameField.style.maxHeight = '0';
+   title.innerHTML="Sign In";
+   text.innerHTML="Forgot Password ";
+   signUpBtn.classList.add('disable');
+   signInBtn.classList.remove('disable');
+});
+
+signUpBtn.addEventListener('click',()=>{
+    nameField.style.maxHeight = '60px';
+    title.innerHTML="Sign Up";
+    text.innerHTML="Password Suggestions ";
+    signInBtn.classList.add('disable');
+    signUpBtn.classList.remove('disable');
+ });
+
